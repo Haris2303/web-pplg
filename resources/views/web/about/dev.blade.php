d7ad8af571c14c409ef3a1e2985302e9550511a3
haris2303 <hrsccf102@gmail.com>
2024-01-18 11:49:55 +0900

    Add profile haris.jpeg

--- resources/views/web/about/dev.blade.php
+++ resources/views/web/about/dev.blade.php
@@ -54,9 +54,8 @@
                     <ul role="list" class="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
                         <li>
                             <div class="flex items-center gap-x-6">
-                                <img class="h-16 w-16 rounded-full"
-                                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    alt="">
+                                <img class="h-16 w-16 rounded-full object-cover" src="/assets/profile/haris.jpeg"
+                                    alt="Haris">
                                 <div>
                                     <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">Haris
                                     </h3>
